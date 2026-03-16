77ee3c04b65f0cd3db52ef12f33cd9a917de5c1d
Lucas Marques <66765396+luqmarqs@users.noreply.github.com>
2026-03-16 18:02:56 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,10 +19,7 @@ lgpd:false
 
 const [cpfErro,setCpfErro]=useState("")
 const [showPrivacy,setShowPrivacy]=useState(false)
-const [assinaturas,setAssinaturas]=useState(() => {
-  const cache = localStorage.getItem("contador-assinaturas")
-  return cache ? Number(cache) : null
-})
+const [assinaturas,setAssinaturas]=useState(0)
 const [telefoneErro,setTelefoneErro]=useState("")
 const [emailErro,setEmailErro] = useState("")
 
